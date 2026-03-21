9ba6683c8e894ec84e2bae67e9b951695fa734fe
Brandon Fowler <brandfowl@gmail.com>
2026-03-21 16:49:49 -0400

    Merge classes properly

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,11 @@
 import { HTMLAttributes } from "preact";
+import { twMerge } from "tailwind-merge";
 
 export const Button = ({ children, ...props }: HTMLAttributes<HTMLButtonElement>) => {
 	return (
 		<button
 			{...props}
-			className="border bg-neutral-700 rounded p-1 min-w-10 hover:bg-neutral-600 hover:cursor-pointer"
+			class={twMerge("border bg-neutral-700 rounded p-1 min-w-10 hover:bg-neutral-600 hover:cursor-pointer", props.class as string)}
 		>
 			{children}
 		</button>
