001521415f841e9e7ee824b7cb721bf510086ab8
Brandon Fowler <brandfowl@gmail.com>
2026-04-23 04:02:58 -0400

    Remove unbreak

--- src/components/form/ImportedCharacter.tsx
+++ src/components/form/ImportedCharacter.tsx
@@ -6,7 +6,7 @@ export const ImportedCharacter = <T, U>(props: {
 	getIconPath: (avatarResource: T, avatarId: number) => string | undefined;
 	avatarId: number
 }) => {
-	const name = props.getNickname(props.avatarResource, props.locResource, props.avatarId);
+	const name = props.getNickname(props.avatarResource, props.locResource, props.avatarId).replace(/<unbreak>(.*?)<\/unbreak>/g, "$1");
 	const icon = props.getIconPath(props.avatarResource, props.avatarId);
 
 	return <div class="flex items-center gap-2">
