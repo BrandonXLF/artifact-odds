9b9584c9e35ea9e1d0812c4f55a13160391b8599
Brandon Fowler <brandfowl@gmail.com>
2026-07-01 21:08:20 -0400

    Restrict scope

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import { Main } from './Main';
 
 export const App = () => {
 	return (
-		<LocationProvider scope={/^(?!\/[^/]*\/documents).*$/}>
+		<LocationProvider scope={/(\/artifact-odds|\/relic-odds)(?!\/documents)/}>
 			<Router>
 				<Main path="/:baseUrl/*" default />
 			</Router>
