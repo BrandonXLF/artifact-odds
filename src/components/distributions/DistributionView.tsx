e69ecd38c897acae8a5b251d062d278e373320d6
Brandon Fowler <brandfowl@gmail.com>
2026-07-01 22:28:37 -0400

    Show values

--- src/components/distributions/DistributionView.tsx
+++ src/components/distributions/DistributionView.tsx
@@ -4,16 +4,17 @@ export const DistributionView = (props: {
 	const maxVal = Math.max(...props.entries.map(([_, val]) => val));
 
 	const relativeEntries = props.entries.map(([name, val]) => {
-		return [name, val / maxVal] as const;
+		return [name, val / maxVal, val] as const;
 	});
 
 	return (
-		<div class="grid grid-cols-[auto_1fr] w-full gap-x-3 items-center">
-			{relativeEntries.map(([name, val]) => (
+		<div class="grid grid-cols-[auto_auto_1fr] w-full gap-x-2 items-center">
+			{relativeEntries.map(([name, relVal, val]) => (
 				<>
 					<span class="font-mono text-nowrap text-right">{name}</span>
+					<div>({val})</div>
 					<div>
-						<div class="h-4 bg-primary-light" style={{ width: `${val * 100}%` }}></div>
+						<div class="h-4 bg-primary-light" style={{ width: `${relVal * 100}%` }}></div>
 					</div>
 				</>
 			))}
