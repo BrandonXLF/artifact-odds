787fcc2df8fad8eaa496bb4f1b9c5a91b7e4258a
Brandon Fowler <brandfowl@gmail.com>
2026-07-18 19:35:00 -0400

    Remove import

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,10 +1,4 @@
-/**
- * Constants common to all supported games. Can be move to
- * {@link GameData} if required in the future.
- * 
- */
-import { GameData } from "./data";
-
+// Constants common to all supported games. Can be move to GameData if required in the future.
 export const SUB_STAT_COUNT = 4;
 export const LOWER_ROLL_COUNT = 4;
 export const UPPER_ROLL_COUNT = 5;
